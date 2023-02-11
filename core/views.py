24125f7610bc21122e83eb6807f1c29301f264ed
connorGA <ronnoccap@gmail.com>
2023-02-10 19:01:51 -0800

    chore: import django auth decorator login _required to addd to some views

--- core/views.py
+++ core/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Profile
 
 # Create your views here.
+
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
 
