7ee961838fe787b84023f014c082b8ad1664e8a5
connorGA <ronnoccap@gmail.com>
2023-02-12 19:20:58 -0800

    feat: add like_post view so user can like and unlike posts

--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Profile, Post
+from .models import Profile, Post, LikePost
 
 # Create your views here.
 
@@ -33,7 +33,25 @@ def upload(request):
 
 @login_required(login_url='signin')
 def like_post(request):
-    return redirect('/')
+    username = request.user.username
+    post_id = request.GET.get('post_id')
+
+    post = Post.objects.get(id=post_id)
+
+    like_filter = LikePost.objects.filter(post_id=post_id, username=username).first()
+    
+    if like_filter == None:
+        new_like = LikePost.objects.create(post_id=post_id, username=username)
+        new_like.save()
+        post.no_of_likes = post.no_of_likes+1
+        post.save()
+        return redirect('/')
+    else:
+        like_filter.delete()
+        post.no_of_likes = post.no_of_likes-1
+        post.save()
+        return redirect('/')
+
 
 
 @login_required(login_url='signin')
